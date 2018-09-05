296a828995b443da7a0bf52e8ddea90f1133e041
Diego Martinez <itsmeyeyo@gmail.com>
2018-09-04 20:28:32 -0500

    Node updates

--- dist/node/api_server.js
+++ dist/node/api_server.js
@@ -1,7 +1,4 @@
 //TODO: Add documentation, add more functionality, and maybe add some type of caching for listings.
-
-const API_KEY = "API KEY HERE";
-const SECRET_KEY = "SECRET KEY HERE";
 const URL = {
 	check_validity: '',
 	get_customers: 'customers', 
@@ -10,81 +7,55 @@ const URL = {
 	create_booking: 'bookings'
 };
 
-const axios = require('axios');
 const restify = require('restify');
 const server = restify.createServer();
-const bookeo = axios.create({
-	baseURL: 'https://api.bookeo.com/v2/',
-	responseType: 'json',
-	headers: {
-		'Content-Type' : 'application/json',
-		'X-Bookeo-apiKey' : API_KEY,
-		'X-Bookeo-secretKey' : SECRET_KEY
-	}
-});
+
+const network = require('./dist/network');
+//network.updateCustomerList();
 
 server.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
-  return next();
+  	return next();
 });
 
 server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
 
-var customers;
-
-function auth(username, password){
-	bookeo.get(URL.get_customers).then((response)=>{
-		customers = response.data;
-		
-		for(let customer in customers.data){
-			if(customer.emailAddress === username){
-				bookeo.get(`customers/{customer.id}/auth`, {
-					params: password
-				}).then(response =>{
-					res.send(customer);
-				}).catch(err=>{
-					res.send(403);
-				});
-			}
-		}
-	}).catch(err=>{
-		res.send(403);
-	});
-}
-
-function apiCall(server, url, params){
-	bookeo.get(url, {
-		params: params
-	}).then((response)=>{
-		server.res.send(response.data);
-		return next();
-	}).catch(err=>{
-		server.res.send('Error has occurred');
-		console.error(err);
-		return next();
-	});
-}
+server.get('/auth/user', (req, res, next)=>{
+	network.authUser(req.query.username, req.query.password, next);
+});
 
 server.get('/get/customers', (req, res, next)=>{
-	apiCall({req:req, res: res, next: next}, URL.get_customers);
+	network.request({req:req, res:res, next:next}, URL.get_customers);
 })
 server.get('/get/classes', (req, res, next)=>{
-	apiCall({req:req, res: res, next: next}, URL.get_products);
+	network.request({req:req, res:res, next:next}, URL.get_products);
 });
 
 server.get('/validate/promo', (req, res, next)=>{
-	apiCall({req:req, res: res, next: next}, URL.check_validity);
-})
+	network.request({req:req, res:res, next:next}, URL.check_validity);
+});
 
 server.get('/create/booking',(req,res,next)=>{
-	apiCall({req:req, res: res, next: next}, URL.create_booking, req.data);
+	network.request({req:req, res:res,next:next}, URL.create_booking, req.data);
+});
 
-})
+server.post('/create/customer', (req, res, next)=>{
+	console.log(req.body);
+	network.createCustomer(req.body).then(response=>{
+		server.res.send(200);
+		this.updateCustomerList();
+		return server.next();
+	}).catch(err=>{
+		server.res.send(err);
+        return server.next();
+	});
+});
+
 server.get('/get/availability', (req, res, next)=>{
-	console.log(req.query);
-	apiCall({req:req, res: res, next: next}, URL.get_availability_slots, req.query);
+	network.request({req:req, res:res, next:next}, URL.get_availability_slots, req.query);
 });
 
 server.listen(3000, ()=>{
 	console.log("Server is running at:", server.url);
-})
+});
---
+++ dist/node/dist/network.js
@@ -0,0 +1,126 @@
+var axios = require('axios');
+
+const API_KEY = ''
+const SECRET_KEY = '';
+
+const bookeo = axios.create({
+	baseURL: 'https://api.bookeo.com/v2/',
+	responseType: 'json',
+	headers: {
+		'Content-Type' : 'application/json',
+		'X-Bookeo-apiKey' : API_KEY,
+		'X-Bookeo-secretKey' : SECRET_KEY
+	}
+});
+
+/**
+ * Stores all network related functions and data.
+ * @module Network
+ */
+var network = {
+    data: {
+        customers: []
+    },
+
+    status: {
+
+    },
+    
+    /**
+     * @memberof Network
+     * @function authUser
+     * @param {String} userrname
+     * @param {String} password
+     * @param {Function} next
+     */
+    authUser: function(username, password, next){
+        for(let customer of this.data.customers){
+            if(customer.emailAddress == username){
+                bookeo.get(`customers/{customer.id}/auth`, {
+					params: password
+				}).then(response =>{
+                    if(response.status == 200){
+                        res.send(customer);
+                        return next();
+                    }
+                    else{
+                        res.send(403);
+                        return next();
+                    }
+				}).catch(err=>{
+                    res.send(403);
+                    return next();
+				});
+            }
+        }
+    },
+
+    /**
+     * @memberof Network
+     * @function createBooking
+     */
+    createBooking: function(server, booking){
+        bookeo.post('bookings', {
+            data: booking
+        }).then(response=>{
+            //Do booking shit here
+            server.res.send(200);
+            return server.next();
+        }).catch(err=>{
+            console.log(err.response);
+            server.res.send(400);
+            return server.next();
+        });
+    },
+
+    /**
+     * @memberof Network
+     * @function createCustomer
+     * @param {Customer} customer
+     * @returns {Promise}
+     */
+    createCustomer: function(customer){
+        return bookeo.post('customers', {
+            data: customer
+        });
+    },
+
+    /**
+     * @memberof Network
+     * @function request
+     * @param {Object} server
+     * @param {String} url
+     * @param {String} params
+     * @param {String} data
+     */
+
+    request: function(server, url, params, data){
+        bookeo.get(url, {
+            params: params,
+            data: data
+        }).then((response)=>{
+            server.res.send(response.data);
+            return server.next();
+        }).catch(err=>{
+            server.res.send(err.response.data);
+            console.error(err);
+            return server.next();
+        });
+    },
+
+    /**
+     * Updates the customer list. Use when updating the customer 
+     * roster so that changes are reflected locally.
+     * @memberof Network
+     * @function updateCustomerList
+     */
+    updateCustomerList: function(){
+        bookeo.get('customers').then(response=>{
+            this.data.customers = response.data.data;
+        }).catch(err=>{
+           console.log(err.response.data);
+        });
+    }
+}
+
+module.exports = network;
